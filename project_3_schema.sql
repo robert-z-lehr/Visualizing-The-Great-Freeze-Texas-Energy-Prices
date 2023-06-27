87b55d0381852d7414690e1deefbb82ed7096884
Jenny Friesenhahn <125410153+JenFreeze@users.noreply.github.com>
2023-06-27 17:47:59 -0500

    minor updates to remove duplicates

--- project_3_schema.sql
+++ project_3_schema.sql
@@ -1,15 +1,9 @@
-DROP TABLE IF EXISTS Producer_Types, Energy_Sources, States, Generating_Capacity, Sectors, Entities, Plants, Stat_descr, Generators, Prices CASCADE;
-
--- Create Producer Types table
-CREATE TABLE Producer_Types (
-    producer_type_id VARCHAR(3) PRIMARY KEY NOT NULL,
-    producer_type_description VARCHAR(30) NOT NULL
-);
+DROP TABLE IF EXISTS Energy_Sources, States, Sectors, Entities, Plants, Stat_descr, Generators, Prices, Processes, Series CASCADE;
 
 -- Create Energy Sources table
 CREATE TABLE Energy_Sources (
     energy_source_id VARCHAR(6) PRIMARY KEY NOT NULL,
-    energy_source_description VARCHAR(30) NOT NULL
+    energy_source_description VARCHAR(50) NOT NULL
 );
 
 -- Create States table
@@ -18,19 +12,6 @@ CREATE TABLE States (
     state_description VARCHAR(30) NOT NULL
 );
 
--- Create Generating Capacity table
-CREATE TABLE Generating_Capacity (
-    id INTEGER PRIMARY KEY NOT NULL,
-	period INTEGER NOT NULL,
-    stateID VARCHAR(2) NOT NULL,
-    producer_type_id VARCHAR(3) NOT NULL,
-    energy_source_id VARCHAR(6) NOT NULL,
-    capability FLOAT(53) NOT NULL,
-    capability_units VARCHAR(15) NOT NULL,
-	FOREIGN KEY (stateID) REFERENCES States(stateID),
-	FOREIGN KEY (producer_type_id) REFERENCES Producer_Types(producer_type_id),
-	FOREIGN KEY (energy_source_id) REFERENCES Energy_Sources(energy_source_id)
-);
 
 -- Create Sectors table
 CREATE TABLE Sectors (
@@ -68,6 +49,8 @@ CREATE TABLE Generators (
     technology VARCHAR NOT NULL,
     energy_source_code VARCHAR NOT NULL,
     prime_mover_code VARCHAR NOT NULL,
+	balancing_authority_code VARCHAR,
+	balancing_authority_name VARCHAR,
     status_descr VARCHAR(2) NOT NULL,
     latitude FLOAT(53) NOT NULL,
     longitude FLOAT(53) NOT NULL,
@@ -75,7 +58,20 @@ CREATE TABLE Generators (
 	FOREIGN KEY (entityid) REFERENCES Entities(entityid),
 	FOREIGN KEY (plantid) REFERENCES Plants(plantid),
 	FOREIGN KEY (sector) REFERENCES Sectors(sector),
-	FOREIGN KEY (status_descr) REFERENCES Stat_Descr(stat_id)
+	FOREIGN KEY (status_descr) REFERENCES Stat_Descr(stat_id),
+	FOREIGN KEY (energy_source_code) REFERENCES Energy_Sources(energy_source_id)
+);
+
+-- Create Series table
+CREATE TABLE Series (
+    series VARCHAR(15) PRIMARY KEY NOT NULL,
+    series_description VARCHAR(100) NOT NULL
+);
+
+-- Create Processes table
+CREATE TABLE Processes (
+    process_id VARCHAR(3) NOT NULL,
+    process_name VARCHAR(50) PRIMARY KEY NOT NULL
 );
 
 -- Create Prices table
@@ -84,24 +80,25 @@ CREATE TABLE Prices (
 	period VARCHAR(7) NOT NULL,
 	area_name VARCHAR(10) NOT NULL,
 	product_name VARCHAR(20) NOT NULL,
-	process VARCHAR(3) NOT NULL,
 	process_name VARCHAR(60) NOT NULL,
 	series VARCHAR(10) NOT NULL,
-	series_description VARCHAR(100) NOT NULL,
 	value FLOAT(53) NOT NULL,
-	units VARCHAR(5) NOT NULL
+	units VARCHAR(5) NOT NULL,
+	FOREIGN KEY (series) REFERENCES Series(series),
+	FOREIGN KEY (process_name) REFERENCES Processes(process_name),
+	FOREIGN KEY (area_name) REFERENCES States(stateID)
 );
 
 -- Confirm tables are populated correctly
-SELECT * FROM Producer_Types;
 SELECT * FROM Energy_Sources;
 SELECT * FROM States;
-SELECT * FROM Generating_Capacity;
 SELECT * FROM Generators;
 SELECT * FROM Sectors;
 SELECT * FROM Entities;
 SELECT * FROM Plants;
 SELECT * FROM Stat_Descr;
 SELECT * FROM Prices;
+SELECT * FROM Processes;
+SELECT * FROM Series;
 
 -- CSV files imported into tables using Import/Export menu option
