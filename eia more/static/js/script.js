c1738829d01fd3ef8f39fd70e8cb9120d135a9c2
ollayyy <124837569+ollayyy@users.noreply.github.com>
2023-06-28 19:41:28 -0500

    Delete eia more/static directory

--- eia more/static/js/script.js
+++
@@ -1,6 +0,0 @@
-function changeGraph() {
-    var selector = document.getElementById('graphSelector');
-    var image = document.getElementById('graphImage');
-    image.src = selector.value;
-    console.log("Image src: " + image.src);
-}
